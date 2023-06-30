acd63d172744be35b69f94be5f40859b192ba13c
Guillaume Smet <guillaume.smet@gmail.com>
2023-06-30 11:49:32 +0200

    test

--- .github/test.groovy
+++ .github/test.groovy
@@ -3,7 +3,10 @@ import groovy.json.JsonSlurper
 if (System.env.GITHUB_ACTIONS) {
     def jobName = System.env.GITHUB_JOB
 
-    if (System.env.GITHUB_EVENT_NAME == "pull_request" && System.env.GITHUB_EVENT_PATH != null) {
+    System.out.println("PULL_REQUEST_NUMBER_ENV_GLOBAL: " + System.env.PULL_REQUEST_NUMBER_ENV_GLOBAL);
+    System.out.println("PULL_REQUEST_NUMBER_ENV_LOCAL: " + System.env.PULL_REQUEST_NUMBER_ENV_LOCAL);
+
+    /*if (System.env.GITHUB_EVENT_NAME == "pull_request" && System.env.GITHUB_EVENT_PATH != null) {
         File eventJsonFile = new File(System.env.GITHUB_EVENT_PATH)
         if (Files.exists(eventJsonFile)) {
             def eventJson = new JsonSlurper().parse(eventJsonFile)
@@ -13,5 +16,5 @@ if (System.env.GITHUB_ACTIONS) {
                 System.out.println("PR NUMBER: " + prNumber)
             }
         }
-    }
+    }*/
 }
